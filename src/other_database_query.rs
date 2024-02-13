a892e82c2271a02665ed8d5a533c3c42de37f532
Daniel Hawkins <dimpdash@gmail.com>
2024-02-13 15:59:03 +1100

    clean up

--- src/other_database_query.rs
+++ src/other_database_query.rs
@@ -1,105 +1,24 @@
-use async_trait::async_trait;
 use bevy_ecs::{component::Component, prelude::*, system::SystemParam};
 use bevy_mod_index::prelude::*;
-use bevy_reflect::Map;
 use bevy_utils::hashbrown::HashSet;
-use sqlx::Sqlite;
 use crate::database_resource::*;
 use crate::database_entity::{DatabaseEntity, DatabaseEntityIndex};
 use crate::database_resource::DatabaseResource;
 
-// stop type warning
-pub type Pool = sqlx::Pool<sqlx::Sqlite>;
-
-#[async_trait]
-pub trait DatabaseQueryInfo: Sized {
-    type Item;
+pub trait DBQueryInfo {
+    // the returned item
+    type Item<'a>;
     type Database: DatabaseResource;
-    type Index: IndexInfo;
+    type Mapper: ComponentMapper;
 
-    fn get<'a>(
-        db: &ResMut<Self::Database>,
-        world: &mut World,
-    ) -> Result<&'a Self::Item, ()>;
-}
-
-pub struct DatabaseQueryFetchState<'w, 's, I: DatabaseQueryInfo + 'static> {
-    db_state: <ResMut<'w, I::Database> as SystemParam>::State,
-    phantom: std::marker::PhantomData<&'s ()>,
-}
-
-pub struct DatabaseQuery<'w, 's, I: DatabaseQueryInfo + 'static> {
-    pub db: ResMut<'w, I::Database>,
-    world: bevy_ecs::world::unsafe_world_cell::UnsafeWorldCell<'w>,
-    phantom: std::marker::PhantomData<&'s ()>,
-}
-
-// pub type RODatabaseQueryItem<'a, I> = &'a I::Component;
-
-pub trait QueryFn<Component>:
-    FnOnce(&mut sqlx::SqliteConnection) -> Result<Vec<(DatabaseEntity, Component)>, ()>
-{
-}
-
-impl<'w, 's, I: DatabaseQueryInfo> DatabaseQuery<'w, 's, I> {
-
-    pub fn get(&mut self, db_entity: &DatabaseEntity) -> Result<&I::Item, ()> {
-        unsafe {
-            I::get(&self.db, self.world.world_mut())
-        }
-    }
-
-}
-
-unsafe impl<'w, 's, I: DatabaseQueryInfo> SystemParam for DatabaseQuery<'w, 's, I>
-where
-    I: DatabaseQueryInfo + 'static,
-{
-    type State = DatabaseQueryFetchState<'static, 'static, I>;
-
-    type Item<'_w, '_s> = DatabaseQuery<'_w, '_s, I>;
-
-    fn init_state(
-        world: &mut World,
-        system_meta: &mut bevy_ecs::system::SystemMeta,
-    ) -> Self::State {
-        // https://github.com/chrisjuchem/bevy_mod_index/blob/15e9b4c9bbf26d4fc087ce056b07d1312464de2f/src/index.rs#L108
-        if !world.contains_resource::<AnyDatabaseResource>() {
-            world.init_resource::<AnyDatabaseResource>();
-        }
-
-        DatabaseQueryFetchState {
-            db_state: <ResMut<'w, I::Database>>::init_state(world, system_meta),
-            phantom: std::marker::PhantomData,
-        }
-    }
-
-    unsafe fn get_param<'w2, 's2>(
-        state: &'s2 mut Self::State,
-        system_meta: &bevy_ecs::system::SystemMeta,
-        world: bevy_ecs::world::unsafe_world_cell::UnsafeWorldCell<'w2>,
-        change_tick: bevy_ecs::component::Tick,
-    ) -> Self::Item<'w2, 's2> {
-        let db_query = DatabaseQuery {
-            db: <ResMut<'w2, I::Database>>::get_param(
-                &mut state.db_state,
-                system_meta,
-                world,
-                change_tick,
-            ),
-            world,
-            phantom: std::marker::PhantomData,
-        };
-
-        db_query
-    }
+    fn get<'w>(db: &mut Self::Database, world: bevy_ecs::world::unsafe_world_cell::UnsafeWorldCell<'w>, db_entity: &DatabaseEntity) -> Result<Self::Item<'w>, ()>;
 }
 
 
 
 pub type DBQueryItem<'a, Q> = <Q as DBQueryInfo>::Item<'a>;
 
-pub struct QueryFetchState<'w, 's, I: DatabaseQueryInfo + 'static> {
+pub struct QueryFetchState<'w, 's, I: DBQueryInfo + 'static> {
     db_state: <ResMut<'w, I::Database> as SystemParam>::State,
     phantom: std::marker::PhantomData<&'s ()>,
 }
@@ -114,7 +33,7 @@ pub struct Query<'world, 'state, Q: DBQueryInfo>  {
 // So query can be constructed by the system
 unsafe impl<'w, 's, I: DBQueryInfo> SystemParam for Query<'w, 's, I>
 where
-    I: DatabaseQueryInfo + 'static,
+    I: DBQueryInfo + 'static,
 {
     type State = QueryFetchState<'static, 'static, I>;
 
@@ -165,15 +84,6 @@ pub trait ComponentMapper {
     fn get<'c, E>(e : E, db_entity: &DatabaseEntity) -> Result<Self::Item, ()>
     where
         E: sqlx::Executor<'c, Database = sqlx::Sqlite>;
-}
-
-pub trait DBQueryInfo {
-    // the returned item
-    type Item<'a>;
-    type Database: DatabaseResource;
-    type Mapper: ComponentMapper;
-
-    fn get<'w>(db: &mut Self::Database, world: bevy_ecs::world::unsafe_world_cell::UnsafeWorldCell<'w>, db_entity: &DatabaseEntity) -> Result<Self::Item<'w>, ()>;
 }
 
 // To satisfy the type system when a DBQueryInfo is composed of other DBQueryInfos
@@ -253,7 +163,7 @@ simple_composition_of_db_queries!{A B C D E F G H I J K}
 
 pub struct UserMapper;
 impl ComponentMapper for UserMapper {
-    type Item = (i64);
+    type Item = i64;
 
     fn get<'c, E>(_e : E, _db_entity: &DatabaseEntity) -> Result<Self::Item, ()>
     where
